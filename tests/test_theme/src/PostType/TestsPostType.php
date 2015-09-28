d8c465c774734360bd356bb89fe8cc1cf7c72487
cjquinn <christy@wearelighthouse.com>
2015-09-28 18:01:03 +0100

    Test post type

--- tests/test_theme/src/PostType/TestsPostType.php
+++ tests/test_theme/src/PostType/TestsPostType.php
@@ -6,4 +6,14 @@ use BestKebab\PostType\PostType;
 
 class TestsPostType extends PostType
 {
+
+    /**
+     * @return void
+     */
+    public function initialise()
+    {
+        $this->addMetaBox('testBox', 'Test Box');
+        $this->addField('test_field', 'text', 'Test Field', 'testBox');
+        parent::initialise();
+    }
 }
